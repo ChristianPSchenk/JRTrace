0cf2de5aa5795c556eb248cba845c340e3630de0
ChristianPSchenk <christian.p.schenk@gmail.com>
2015-05-11 20:49:48 +0200

    Forgot to add the new test

--- de.schenk.jrtrace.service.test/src/de/schenk/jrtrace/service/test/AllTests.java
+++ de.schenk.jrtrace.service.test/src/de/schenk/jrtrace/service/test/AllTests.java
@@ -13,6 +13,7 @@ import de.schenk.jrtrace.enginex.testscripts.EngineXDetailsTest;
 import de.schenk.jrtrace.enginex.testscripts.EngineXLifeCycleTest;
 import de.schenk.jrtrace.helperagent.TypeCheckUtilTest;
 import de.schenk.jrtrace.helperlib.tests.InspectUtilTest;
+import de.schenk.jrtrace.helperlib.tests.ReflectionUtilTest;
 import de.schenk.jrtrace.service.JRTraceConnectToLaunchedAgentTest;
 import de.schenk.jrtrace.service.JRTraceControllerTest;
 import de.schenk.jrtrace.service.test.utils.JavaUtilTest;
@@ -22,7 +23,7 @@ import de.schenk.jrtrace.service.test.utils.JavaUtilTest;
 		EngineXAnnotationReaderTest.class, EngineXLifeCycleTest.class,
 		JRTraceControllerTest.class, JavaUtilTest.class, InspectUtilTest.class,
 		EngineXDetailsTest.class, TypeCheckUtilTest.class,
-		SimpleReinstallTest.class })
+		SimpleReinstallTest.class, ReflectionUtilTest.class })
 public class AllTests {
 
 }
